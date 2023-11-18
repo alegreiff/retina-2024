9b6f338d551280662dc9a2ce14594433da350352
loorlab <labloor@gmail.com>
2023-11-18 14:18:55 -0500

    Add Retina Latina Metadata Plugin V1.0

---
+++ wp-content/mu-plugins/retina-metadata.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name: Retina Latina Metadata
+ * Plugin URI: https://www.retinalatina.org/
+ * Description:  Metadatos para Retina Latina
+ * Version: 1.0
+ * Author: loorlab
+ * Author URI: https://www.loorlab.com/
+ *
+ * @package RetinaLatina24
+ * @author loorlab <https://www.loorlab.com/>
+ */
+
+// Quick exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
